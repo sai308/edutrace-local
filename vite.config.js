80e80eda06b11fa8f710839c1a2bc4ff26fbfeed
Andrew Sai <andrii.saienko@uzhnu.edu.ua>
2025-11-24 01:44:01 +0200

    fix: Update Cloudflare Vite plugin import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import cloudflare from '@cloudflare/vite-plugin'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
